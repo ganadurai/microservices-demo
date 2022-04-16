1df640a2364af70133b08014b8a6807846b50a19
Ganapathy Duraiswamy <ganadurai@google.com>
2022-04-15 23:15:12 -0500

    fixes

--- api-client-java/src/main/java/com/google/apigee/onlineboutique/RestServiceApplication.java
+++ api-client-java/src/main/java/com/google/apigee/onlineboutique/RestServiceApplication.java
@@ -3,10 +3,12 @@ package com.google.apigee.onlineboutique;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.google.apigee.onlineboutique.productcatalog.ProductCatalogController;
+
 @SpringBootApplication
 public class RestServiceApplication {
     public static void main(String[] args) {
-        //SpringApplication.run(HelloWorldController.class, args);
+        SpringApplication.run(ProductCatalogController.class, args);
     }
 
 }
